8be8de7fb196eacb7285e350192c8f13af313da0
Idowu Emmanuel <idowu.immanuel17@gmail.com>
2025-02-15 21:21:39 +0100

    changes

--- resources/views/multiple-options.blade.php
+++ resources/views/multiple-options.blade.php
@@ -21,65 +21,70 @@
             margin: 10px;
         }
     </style>
-    <div class="card radius-10 ml-auto mr-auto mt-4 col-md-10 col-sm-11 col-lg-9 col-xl-8">
-        <div class="card-body px-lg-4 px-md-3  px-xl-5 pt-lg-4 pt-md-3 pt-xl-5 ">
-            <div style="min-height: 140px;"
-                 class="d-flex justify-content-center align-items-center bg-gray radius-5 p-2 fade-in">
-                <p id="question" class="p-2 text-center text-black-50">
+    <div class="row row-no-gutter">
 
+
+
+        <div class="card radius-10 ml-auto mr-auto mt-4 col-md-10 col-sm-11 col-lg-9 col-xl-8">
+            <div class="card-body px-lg-4 px-md-3 px-xl-5 py-3 pt-xl-5">
+                <div style="min-height: 140px;"
+                     class="d-flex justify-content-center align-items-center bg-gray radius-5 p-2 fade-in">
+                    <p id="question" class="p-2 text-center text-black-50">
+                    </p>
+                </div>
+                <div class="d-flex justify-content-center align-items-center p-2 text-center">
+                    <h4 id="timer" class="text-center col-6">00:00</h4>
+                    <h4 id="counter" class="text-center col-6"></h4>
+                </div>
+
+                <div class="row row-no-gutter">
+                    <label for="a" class="form-check fade-in col-md-6">
+                        <input class="form-check-input" type="radio" name="option" onchange="selectOption('a')" id="a"
+                               value="a">
+                        <label class="form-check-label" for="a">
+                            <p id="option1"></p>
+                        </label>
+                    </label>
+                    <label for="b" class="form-check fade-in  col-md-6">
+                        <input class="form-check-input" type="radio" id="b" onchange="selectOption('b')" name="option"
+                               value="b">
+                        <label class="form-check-label" for="b">
+                            <p id="option2"></p>
+                        </label>
+                    </label>
+                    <label for="c" class="form-check fade-in  col-md-6">
+                        <input class="form-check-input" type="radio" onchange="selectOption('c')" id="c" name="option"
+                               value="c">
+                        <label class="form-check-label" for="c">
+                            <p id="option3"></p>
+                        </label>
+                    </label>
+                    <label for="d" class="form-check fade-in  col-md-6">
+                        <input class="form-check-input" id="d"
+                               onchange="selectOption('d')" type="radio" value="d" name="option">
+                        <label class="form-check-label" for="d">
+                            <p id="option4"></p>
+                        </label>
+                    </label>
+                    <label for="e" class="form-check fade-in  col-md-6 ">
+                        <input class="form-check-input" onchange="selectOption('e')" id="e" type="radio" name="option"
+                               value="e">
+                        <label class="form-check-label" for="e">
+                            <p id="option5"></p>
+                        </label>
+                    </label>
+                </div>
+            </div>
+            <div class="px-3  d-md-flex px-lg-4 px-md-3 px-xl-5 pb-2 justify-content-between">
+                <p class="text-left text-danger " style="font-size: 60%">
+                    Be sure before you check an answer, You only get one chance at it
+                </p>
+                <p class="text-md-right" style="font-size: 60%">
+                    Name: {{$student->name??''}} &nbsp;
+                    Type: {{$student->type??''}} &nbsp;
+                    Level: {{$student->level??''}} &nbsp;
                 </p>
             </div>
-            <div class="d-flex justify-content-center align-items-center p-2 text-center">
-                <h4 id="timer" class="text-center col-6">00:00</h4>
-                <h4 id="counter" class="text-center col-6"></h4>
-            </div>
-            <div class="row px-5">
-                <label for="a" class="form-check fade-in  col-md-6">
-                    <input class="form-check-input" type="radio" name="option" onchange="selectOption('a')" id="a"
-                           value="a">
-                    <label class="form-check-label" for="a">
-                        <p id="option1"></p>
-                    </label>
-                </label>
-                <label for="b" class="form-check fade-in  col-md-6">
-                    <input class="form-check-input" type="radio" id="b" onchange="selectOption('b')" name="option"
-                           value="b">
-                    <label class="form-check-label" for="b">
-                        <p id="option2"></p>
-                    </label>
-                </label>
-                <label for="c" class="form-check fade-in  col-md-6">
-                    <input class="form-check-input" type="radio" onchange="selectOption('c')" id="c" name="option"
-                           value="c">
-                    <label class="form-check-label" for="c">
-                        <p id="option3"></p>
-                    </label>
-                </label>
-                <label for="d" class="form-check fade-in  col-md-6">
-                    <input class="form-check-input" id="d"
-                           onchange="selectOption('d')" type="radio" value="d" name="option">
-                    <label class="form-check-label" for="d">
-                        <p id="option4"></p>
-                    </label>
-                </label>
-                <label for="e" class="form-check fade-in  col-md-6 ">
-                    <input class="form-check-input" onchange="selectOption('e')" id="e" type="radio" name="option"
-                           value="e">
-                    <label class="form-check-label" for="e">
-                        <p id="option5"></p>
-                    </label>
-                </label>
-            </div>
-        </div>
-        <div class="d-flex px-lg-4 px-md-3 px-xl-5 pb-2 justify-content-between">
-            <p class="text-left text-danger " style="font-size: 60%">
-                Be sure before you check an answer, You only get one chance at it
-            </p>
-            <p class="text-right" style="font-size: 60%">
-                Name: {{$student->name??''}} &nbsp;
-                Type: {{$student->type??''}} &nbsp;
-                Level: {{$student->level??''}} &nbsp;
-            </p>
         </div>
     </div>
 @stop
